f10fbff1ce008b3e3b6be9e5b365023ba7404644
Hebert França <135760354+devHebertfst@users.noreply.github.com>
2024-07-23 20:31:40 -0300

    new

---
+++ src/main/java/com/example/appspring/model/Role.java
@@ -0,0 +1,4 @@
+package com.example.appspring.model;
+
+public class Role {
+}
---
+++ src/main/java/com/example/appspring/model/User.java
@@ -0,0 +1,7 @@
+package com.example.appspring.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+}
